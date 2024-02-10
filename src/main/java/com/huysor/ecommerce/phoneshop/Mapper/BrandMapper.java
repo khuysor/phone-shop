29bca3afde52f8220f0301ff16f52fd60cc55228
Huysorkheang <xain@Huysorkheangs-MacBook-Pro.local>
2024-02-10 22:41:38 +0700

    modify project to use mapstruct

--- src/main/java/com/huysor/ecommerce/phoneshop/Mapper/BrandMapper.java
+++ src/main/java/com/huysor/ecommerce/phoneshop/Mapper/BrandMapper.java
@@ -1,12 +1,16 @@
 package com.huysor.ecommerce.phoneshop.Mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import com.huysor.ecommerce.phoneshop.dto.BrandDTO;
 import com.huysor.ecommerce.phoneshop.entity.Brands;
 
 @Mapper
 public interface BrandMapper {
+	BrandMapper INSTANCE= Mappers.getMapper(BrandMapper.class);
 
-    Brands toBrand(BrandDTO brandDTO);
+    Brands toBrand(BrandDTO dto);
+
+    BrandDTO toBrandDTO(Brands entity);
 }
--- src/main/java/com/huysor/ecommerce/phoneshop/controller/BrandController.java
+++ src/main/java/com/huysor/ecommerce/phoneshop/controller/BrandController.java
@@ -1,48 +1,47 @@
 package com.huysor.ecommerce.phoneshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.huysor.ecommerce.phoneshop.Mapper.BrandMapper;
 import com.huysor.ecommerce.phoneshop.dto.BrandDTO;
 import com.huysor.ecommerce.phoneshop.entity.Brands;
 import com.huysor.ecommerce.phoneshop.services.BrandService;
-import com.huysor.ecommerce.phoneshop.util.Mapper;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("brand")
 public class BrandController {
-    @Autowired
-    private BrandService brandService;
+	@Autowired
+	private BrandService brandService;
 
-    // post method
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> create(@RequestBody BrandDTO brandDTO){
+	// post method
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> create(@RequestBody BrandDTO brandDTO) {
+		Brands brands = BrandMapper.INSTANCE.toBrand(brandDTO);
+		brands=brandService.create(brands);
+		return ResponseEntity.ok(BrandMapper.INSTANCE.toBrandDTO(brands));
+	}
 
-        Brands brands= Mapper.toBrand(brandDTO);
-        brands = brandService.create(brands);
-        // to respon all data from brands
-//        return ResponseEntity.ok(brands) ;
+	@GetMapping("{id}")
+	public ResponseEntity<?> getOneBrand(@PathVariable("id") Integer brand_id) {
+		Brands brands = brandService.getBrandById(brand_id);
+		return ResponseEntity.ok(BrandMapper.INSTANCE.toBrandDTO(brands));
+	}
 
-        // to respon body with id
-        return ResponseEntity.ok(Mapper.toBrandDTO(brands)) ;
-    }
-    @GetMapping("{id}")
-    public ResponseEntity<?>getOneBrand(@PathVariable("id") Integer brand_id){
-      Brands brands=  brandService.getBrandById(brand_id);
-        return ResponseEntity.ok(Mapper.toBrandDTO(brands));
-    }
-    @PutMapping("{id}")
-    public ResponseEntity<?>updateBrand(@PathVariable("id") Integer brand_id,@RequestBody BrandDTO brandDTO){
-        Brands brands =  Mapper.toBrand(brandDTO);
-        Brands updated= brandService.update(brand_id,brands);
-        return ResponseEntity.ok(Mapper.toBrandDTO(updated));
-    }
+	@PutMapping("{id}")
+	public ResponseEntity<?> updateBrand(@PathVariable("id") Integer brand_id, @RequestBody BrandDTO brandDTO) {
+		Brands brands = BrandMapper.INSTANCE.toBrand(brandDTO);
+		Brands updated = brandService.update(brand_id, brands);
+		return ResponseEntity.ok(BrandMapper.INSTANCE.toBrandDTO(updated));
+	}
+//	@RequestMapping
+//	public ResponseEntity<?>getBrands(@RequestParam Map<String,String>params){
+//
+//
+//	}
 
 }
