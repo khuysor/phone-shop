2669ce792c1b10e0736d73d076ae1cc0694d9688
Khuysor <huysorkheang22@gmail.com>
2024-03-25 09:26:48 +0700

    using and config spring security

--- src/main/java/com/huysor/ecommerce/phoneshop/config/security/SecurityConfig.java
+++ src/main/java/com/huysor/ecommerce/phoneshop/config/security/SecurityConfig.java
@@ -1,22 +1,53 @@
 package com.huysor.ecommerce.phoneshop.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
 
 public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-       http .authorizeHttpRequests().requestMatchers("/","index.html").permitAll()
-                .anyRequest().authenticated().and()
-                .httpBasic();
-       return http.build();
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/", "index.html").permitAll()
+                .requestMatchers("/brand").hasRole("SALE")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+        @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails userDetails1= User
+                .withUsername("kok")
+                .password(passwordEncoder.encode("1234"))
+                .roles("SALE")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails,userDetails1);
     }
 
 
