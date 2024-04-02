0b087c898be5a34fbda901d6ca6a08f1c272ae81
Khuysor <huysorkheang22@gmail.com>
2024-04-02 14:10:53 +0700

    commit somecode

---
+++ src/main/java/com/huysor/ecommerce/phoneshop/config/security/AuthUser.java
@@ -0,0 +1,57 @@
+package com.huysor.ecommerce.phoneshop.config.security;
+
+import lombok.AllArgsConstructor;
+
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AuthUser implements UserDetails {
+    private Set<? extends GrantedAuthority> authorities;
+    private String userName;
+    private String passWord;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean accEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return passWord;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return accEnabled;
+    }
+}
